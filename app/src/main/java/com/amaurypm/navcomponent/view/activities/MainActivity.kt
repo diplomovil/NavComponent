551d0233ce1063a4c56cff0b45c49c135b12c930
AmauryPM <amaury.matsumura@gmail.com>
2022-10-15 14:02:43 -0500

    Commit inicial

---
+++ app/src/main/java/com/amaurypm/navcomponent/view/activities/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.amaurypm.navcomponent.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.amaurypm.navcomponent.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController) //Para usar el actionbar en la navegación
+    }
+}
---
+++ app/src/main/java/com/amaurypm/navcomponent/view/fragments/FormularioFragment.kt
@@ -0,0 +1,24 @@
+package com.amaurypm.navcomponent.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amaurypm.navcomponent.R
+
+class FormularioFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_formulario, container, false)
+    }
+
+}
